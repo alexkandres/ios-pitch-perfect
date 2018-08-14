9de04dc291a31f5c4ebdb781f59c38e794c2261f
Alex Andres <alex.k.andres@disney.com>
2018-08-14 15:40:42 -0400

    IBoutlet label

--- testAppEDT/ViewController.swift
+++ testAppEDT/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelOutlet: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +23,7 @@ class ViewController: UIViewController {
 
     @IBAction func recordAction(_ sender: Any) {
         print("Record button pressed")
+        labelOutlet.text = "new text"
     }
     
 }
